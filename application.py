a3f904ed2457662430a0a2563502ed0e6abd09b2
Aman14-ai <amancode2005@gmail.com>
2026-01-28 20:18:39 +0530

    forest_fire_predictor

---
+++ application.py
@@ -0,0 +1,39 @@
+from flask import Flask,render_template,request,jsonify
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+application=Flask(__name__)
+app=application
+
+# import model
+ridge_model=pickle.load(open('models/ridge.pkl','rb'))
+standard_scaler=pickle.load(open('models/scaler.pkl','rb'))
+
+@app.route('/',methods=['GET','POST'])
+def predict_data():
+    if(request.method=="POST"):
+        print("Entered in form")
+        Temperature=float(request.form.get('Temperature'))
+        RH=float(request.form.get('RH'))
+        Ws=float(request.form.get('Ws'))
+        Rain=float(request.form.get('Rain'))
+        FFMC=float(request.form.get("FFMC"))
+        DMC=float(request.form.get("DMC"))
+        ISI=float(request.form.get("ISI"))
+        Classes=float(request.form.get("Classes"))
+        Region=float(request.form.get("Region"))
+
+        new_data=[[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]]
+
+        new_data_scaled=standard_scaler.transform(new_data)
+
+        result=ridge_model.predict(new_data_scaled)
+        return render_template("home.html",results=result[0])
+    else:
+        return render_template("home.html")
+
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0')
